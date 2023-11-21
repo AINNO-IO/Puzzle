e8ebc3970758ed2f05395600ee88e75a4bb4274a
Modestas Kažinauskas <modestas.kazinauskas@modakoda.com>
2023-11-21 21:03:51 +0200

    Forward from main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,18 @@
-import Image from 'next/image'
+'use server'
+import PuzzleData from '@/puzzle/puzzle-data'
+import { getPuzzleData } from '@/puzzle/puzzle-repository'
+import { redirect } from 'next/navigation'
 
-export default function Home() {
+export default async function Home() {
+  const puzzles: PuzzleData[] = await getPuzzleData()
+  if (puzzles) {
+    const i = Math.floor(Math.random() * puzzles.length)
+    redirect(`/puzzle/${puzzles[i].id}`)
+  } else {
+    redirect(`/admin/upload`)
+  }
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="">
 
     </main>
   )
