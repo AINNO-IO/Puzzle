bae80dfed09fe4f985f26f5629887abb9d431af3
Modestas Kažinauskas <modestas.kazinauskas@modakoda.com>
2023-11-28 00:38:34 +0200

    Preview window

--- src/app/(admin)/admin/upload/form-page.tsx
+++ src/app/(admin)/admin/upload/form-page.tsx
@@ -1,33 +1,37 @@
 'use client'
 import Image from "next/image";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import Link from "next/link";
 import PuzzleData from "@/puzzle/puzzle-data";
 import savePuzzle from "@/puzzle/puzzle-repository";
+import Profile from "@/app/(public)/puzzle/[puzzleId]/profile";
 
 export default function FormPage() {
     const [pictureUrl, setPictureUrl] = useState('')
-
     const [formData, setFormData] = useState({
         id: '',
         title: '',
         text: '',
         pictureFile: null as unknown as File
     })
+    const [showPreview, setShowPreview] = useState(false)
+    const modalRef = useRef(null);
 
-    const pictureElement = pictureUrl ? (
-        <>
-            <Image
-                width={600}
-                height={600}
-                src={pictureUrl}
-                alt='Puzzle picture'
-            />
-        </>
-    )
-        :
-        (<>No picture selected</>)
+    useEffect(() => {
+        if (!modalRef.current) {
+            return;
+        }
+        showPreview ? (modalRef.current as any).showModal() : (modalRef.current as any).close();
+    }, [showPreview]);
 
+    const handleClose = () => {
+        setShowPreview(false)
+    }
+
+    const handleESC = (event: any) => {
+        event.preventDefault();
+        handleClose();
+    }
 
     const generatedLink = formData.id ? (
         <Link href={`/puzzle/${formData.id}`} ><span className="hover:text-red-400">{`${process.env.NEXT_PUBLIC_SITE_URL}/puzzle/${formData.id}`}</span></Link>
@@ -52,77 +56,102 @@ export default function FormPage() {
         savePuzzle(dataToSave).then((uuid) => {
             setFormData({ ...formData, id: uuid })
         })
+    }
 
-    };
+    const allDataExistsForPreview = formData.pictureFile && formData.text && formData.title
 
-    return (<div className="hero m-auto">
-        <div className="hero-content flex-col lg:flex-row-reverse">
+    return (
+        <>
+            <dialog ref={modalRef} id="my_modal_3" className="modal" onCancel={handleESC}>
+                <div className="modal-box w-11/12 max-w-full">
+                    <form method="dialog">
+                        <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2" onClick={() => setShowPreview(false)}>✕</button>
+                    </form>
+                    <h3 className="font-bold text-lg">Preview</h3>
+                    {showPreview ? (<Profile title={formData.title} text={formData.text} imgUrl={pictureUrl} noLink={true} noFullHeight={true} />) : (<></>)}
+                </div>
+            </dialog>
+            <div className="hero m-auto">
+                <div className="hero-content flex-col lg:flex-row-reverse">
 
-            {pictureUrl ? (
-                <div className="text-center lg:text-left w-full">
-                    <div className="flex justify-center px-4 text-xl">Preview</div>
-                    <div className="flex justify-center px-4 pt-4">
-                        {pictureElement}
+                    {/* {pictureUrl ? (
+                        <div className="text-center lg:text-left w-full">
+                            <div className="flex justify-center px-4 text-xl">Preview</div>
+                            <div className="flex justify-center px-4 pt-4">
+                                {pictureElement}
+                            </div>
+                        </div>
+                    ) : null} */}
+
+                    <div className="card shrink-0 w-full max-w-xl shadow-2xl">
+                        <form className="card-body" onSubmit={handleSubmit}>
+
+                            <div className="form-control">
+                                <input
+                                    type="file"
+                                    className="file-input file-input-bordered file-input-primary w-full max-w-xs"
+                                    onChange={(event) => {
+                                        if (!event || !event.target || !event.target.files || !event.target.files[0]) {
+                                            setFormData({ ...formData, pictureFile: null as unknown as File })
+                                            setPictureUrl('')
+                                        } else {
+                                            const file = event.target.files[0]
+                                            setFormData({ ...formData, pictureFile: file })
+                                            setPictureUrl(URL.createObjectURL(file))
+                                        }
+                                    }}
+                                    required
+                                />
+                            </div>
+
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Title</span>
+                                </label>
+                                <input
+                                    type="text"
+                                    accept="image/*"
+                                    className="input input-bordered"
+                                    required
+                                    onChange={(event) => setFormData({ ...formData, title: event.target.value })}
+                                    value={formData.title}
+                                />
+                            </div>
+
+
+                            <div className="form-control">
+                                <label className="label">
+                                    <span className="label-text">Text Points</span>
+                                </label>
+                                <textarea
+                                    onChange={(event) => setFormData({ ...formData, text: event.target.value })}
+                                    value={formData.text}
+                                    className="textarea textarea-bordered textarea-lg"
+                                    placeholder=""
+                                    required
+                                ></textarea>
+                            </div>
+                            {allDataExistsForPreview ?
+                                (
+                                    <div className="form-control mt-6">
+                                        <button className="btn btn-secondary" onClick={(e) => { e.preventDefault(); setShowPreview(true) }}>Preview</button>
+                                    </div>
+                                ) :
+                                (
+                                    <></>
+                                )
+                            }
+
+
+                            {formData.id ? (<p className="text-center">Generated URL: {generatedLink}</p>) : (<></>)}
+                            <div className="form-control mt-6">
+                                <button className="btn btn-primary">Generate puzzle link</button>
+                            </div>
+                        </form>
                     </div>
                 </div>
-            ) : null}
-
-            <div className="card shrink-0 w-full max-w-xl shadow-2xl">
-                <form className="card-body" onSubmit={handleSubmit}>
-
-                    <div className="form-control">
-                        <input
-                            type="file"
-                            className="file-input file-input-bordered file-input-primary w-full max-w-xs"
-                            onChange={(event) => {
-                                if (!event || !event.target || !event.target.files || !event.target.files[0]) {
-                                    setFormData({ ...formData, pictureFile: null as unknown as File })
-                                    setPictureUrl('')
-                                } else {
-                                    const file = event.target.files[0]
-                                    setFormData({ ...formData, pictureFile: file })
-                                    setPictureUrl(URL.createObjectURL(file))
-                                }
-                            }}
-                            required
-                        />
-                    </div>
-
-                    <div className="form-control">
-                        <label className="label">
-                            <span className="label-text">Title</span>
-                        </label>
-                        <input
-                            type="text"
-                            accept="image/*"
-                            className="input input-bordered"
-                            required
-                            onChange={(event) => setFormData({ ...formData, title: event.target.value })}
-                            value={formData.title}
-                        />
-                    </div>
-
-
-                    <div className="form-control">
-                        <label className="label">
-                            <span className="label-text">Text Points</span>
-                        </label>
-                        <textarea
-                            onChange={(event) => setFormData({ ...formData, text: event.target.value })}
-                            value={formData.text}
-                            className="textarea textarea-bordered textarea-lg"
-                            placeholder=""
-                            required
-                        ></textarea>
-                    </div>
-                    {formData.id ? (<p className="text-center">Generated URL: {generatedLink}</p>) : (<></>)}
-                    <div className="form-control mt-6">
-                        <button className="btn btn-primary">Generate puzzle link</button>
-                    </div>
-                </form>
             </div>
-        </div>
-    </div>
+        </>
     )
 }
 
--- src/app/(public)/puzzle/[puzzleId]/profile.tsx
+++ src/app/(public)/puzzle/[puzzleId]/profile.tsx
@@ -3,10 +3,12 @@
 import Image from "next/image"
 import Link from "next/link";
 
-export default function Profile({ imgUrl, title, text }: { imgUrl: string, title: string, text: string }) {
-
+export default function Profile(
+    { imgUrl, title, text, noLink, noFullHeight }:
+        { imgUrl: string, title: string, text: string, noLink?: boolean, noFullHeight?: boolean }
+) {
     return (
-        <div className="hero min-h-screen">
+        <div className={`hero ${noFullHeight ? '' : 'min-h-screen'}`}>
             <div className="card lg:card-side bg-base-100 shadow-xl m-10">
                 <figure>
                     <Image
@@ -21,7 +23,12 @@ export default function Profile({ imgUrl, title, text }: { imgUrl: string, title
                     <h2 className="card-title">{title}</h2>
                     <p>{text.split('\n').map((element, index) => (<span key={index}>{element} <br /></span>))}</p>
                     <div className="card-actions justify-end mt-5">
-                        <Link href='/' passHref><button className="btn btn-primary">New puzzle!</button></Link>
+                        {noLink === true ? (
+                            <button className="btn btn-primary">New puzzle!</button>
+                        ) : (
+                            <Link href='/' passHref><button className="btn btn-primary">New puzzle!</button></Link>
+                        )}
+
                     </div>
                 </div>
             </div>
