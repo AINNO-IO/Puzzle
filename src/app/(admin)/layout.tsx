90f461879630dc4b79a87b7ecf888a7b97d3516e
Modestas Kažinauskas <modestas.kazinauskas@modakoda.com>
2023-12-06 21:36:32 +0200

    Analytics

--- src/app/(admin)/layout.tsx
+++ src/app/(admin)/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import Footer from "./footer";
 import "./../globals.css";
 import Navbar from "./navbar";
+import { Analytics } from '@vercel/analytics/react';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -22,6 +23,7 @@ export default function RootLayout({
         <Navbar />
         {children}
         <Footer />
+        <Analytics />
       </body>
     </html>
   );
